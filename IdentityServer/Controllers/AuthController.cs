8c015518cea7fb640861a715d5e6f7c40319a238
kheneahm <kheneahm9@gmail.com>
2021-02-14 23:11:18 -0600

    Adding register functionality

--- IdentityServer/Controllers/AuthController.cs
+++ IdentityServer/Controllers/AuthController.cs
@@ -13,10 +13,12 @@ namespace IdentityServer.Controllers
     public class AuthController : Controller
     {
         private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AuthController(SignInManager<AppUser> signInManager)
+        public AuthController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
        
         [HttpGet]
@@ -38,6 +40,39 @@ namespace IdentityServer.Controllers
 
             if (result.Succeeded)
             {
+                return Redirect(viewModel.ReturnUrl);
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            return View(new RegisterViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var newUser = new AppUser()
+            {
+                UserName = viewModel.Username
+            };
+
+            var result = await _userManager.CreateAsync(newUser, viewModel.Password);
+
+
+            if(result.Succeeded)
+            {
+                await _signInManager.SignInAsync(newUser, false);
+
+                //redirect back to our servers callback
                 return Redirect(viewModel.ReturnUrl);
             }
 
